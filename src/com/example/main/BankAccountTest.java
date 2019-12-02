e2644264ebe8b81601ba25688eaf2fa3338cf1cd
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2019-12-02 20:23:42 +0100

    Created class BankAccountTests and added testmethods

---
+++ src/com/example/main/BankAccountTest.java
@@ -0,0 +1,41 @@
+package com.example.main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    public void deposit() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void withDraw() {
+        BankAccount account = new BankAccount("Andrew", "Buchalka", 500.00, BankAccount.CHECKING);
+        account.withDraw(200.00, true);
+        assertEquals(300.00, account.getBalance(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void getBalance_deposit() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void getBalance_withDraw() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        account.withDraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void isChecking_true() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        assertTrue(account.isChecking(), "The account is NOT a checking account");
+    }
+
+}
